cf5eb47ba763f60775bb0693b047097e0a6d9383
marshhpc <64800792+marshhpc@users.noreply.github.com>
2020-09-08 22:25:27 -0700

    read me updated

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # book-list
-Small single page application that helped me better my knowledge of object oriented programming and vanilla javaScript 
+Small single page application that helped me better my knowledge of object oriented programming and vanilla javaScript. Added a es6 to show differnce between es5 and es6.
--- appes6.js
+++ appes6.js
@@ -52,3 +52,45 @@ class UI {
     document.getElementById("isbn").value = "";
   }
 }
+
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  // Get form values
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI
+  const ui = new UI();
+
+  // Validate
+  if (title === "" || author === "" || isbn === "") {
+    // Error alert
+    ui.showAlert("Please fill in all fields", "error");
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
+
+    // Show success
+    ui.showAlert("Book Added!", "success");
+
+    // Clear fields
+    ui.clearFields();
+  }
+
+  e.preventDefault();
+});
+
+// Event Listener for Delete
+document.getElementById("book-list").addEventListener("click", function (e) {
+  // Instantiate UI
+  const ui = new UI();
+  // Delete book
+  ui.deleteBook(e.target);
+
+  // Show alert once dleted
+  ui.showAlert("Book Removed!", "success");
+
+  e.preventDefault();
+});
